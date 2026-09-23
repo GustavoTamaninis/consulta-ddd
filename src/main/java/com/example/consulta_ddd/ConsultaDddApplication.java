32277469457c08ed828acd1706269c7ed73941ea
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-09-23 19:47:23 -0300

    chore: adiciona configuração inicial do spring

---
+++ src/main/java/com/example/consulta_ddd/ConsultaDddApplication.java
@@ -0,0 +1,13 @@
+package com.example.consulta_ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/consulta_ddd/ConsultaDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consulta_ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
